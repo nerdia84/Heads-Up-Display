a35173aed806c8b282c02a74a18bc3bb1a311943
nerdia <42793802+nerdia84@users.noreply.github.com>
2021-11-08 09:57:39 +0530

    Add files via upload

---
+++ HUD/src/sensor.js
@@ -0,0 +1,44 @@
+// Geo Location
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude +
+        "<br>Altitude: " + position.coords.longitude;
+}
+
+/*
+var options = {
+    enableHighAccuracy: true,
+    timeout: 3000,
+    maximumAge: 0
+};
+
+function success(pos) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+*/
+
+// Compass 0 - 359
+
+// Atitude  
